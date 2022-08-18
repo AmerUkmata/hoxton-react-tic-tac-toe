0c0e072861560006a41619e090a8389c92415128
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-18 23:41:53 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import styled from 'styled-components';
-import './App.css'
+import { Game } from './components/Game';
 
-type LayoutProps = {
-  gap: number,
-}
-
-const Row = styled.div<LayoutProps>
- display: flex;
- flex-direction: Row;
- gap: $((props)=> props.gap)px;
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Game/>
     </div>
   )
 }
