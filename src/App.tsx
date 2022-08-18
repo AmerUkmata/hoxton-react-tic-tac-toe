10bd4426b8cfe495a8751529d20ea200a3f611e2
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-18 23:35:04 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import reactLogo from './assets/react.svg'
 import styled from 'styled-components';
 import './App.css'
 
+type LayoutProps = {
+  gap: number,
+}
+
+const Row = styled.div<LayoutProps>
+ display: flex;
+ flex-direction: Row;
+ gap: $((props)=> props.gap)px;
+
 function App() {
   const [count, setCount] = useState(0)
 
